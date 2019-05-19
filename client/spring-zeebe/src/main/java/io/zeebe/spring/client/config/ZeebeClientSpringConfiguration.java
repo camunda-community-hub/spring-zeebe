e53da76cfc76e13744959f17758a9ef2a3b5925c
Simone Pulcini <simone.pulcini@gmail.com>
2019-05-19 20:46:19 +0200

    factory and builder methods now fetch properties

--- client/spring-zeebe/src/main/java/io/zeebe/spring/client/config/ZeebeClientSpringConfiguration.java
+++ client/spring-zeebe/src/main/java/io/zeebe/spring/client/config/ZeebeClientSpringConfiguration.java
@@ -1,5 +1,6 @@
 package io.zeebe.spring.client.config;
 
+import io.zeebe.client.ZeebeClientBuilder;
 import io.zeebe.client.impl.ZeebeClientBuilderImpl;
 import io.zeebe.client.impl.ZeebeClientImpl;
 import io.zeebe.spring.client.ZeebeClientLifecycle;
@@ -7,6 +8,8 @@ import io.zeebe.spring.client.ZeebeClientObjectFactory;
 import io.zeebe.spring.client.bean.value.factory.ReadAnnotationValueConfiguration;
 import io.zeebe.spring.client.config.processor.PostProcessorConfiguration;
 import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -14,8 +17,12 @@ import org.springframework.context.annotation.Import;
 @Import({
   PostProcessorConfiguration.class,
   ReadAnnotationValueConfiguration.class,
+
 })
 public class ZeebeClientSpringConfiguration {
+
+  @Autowired
+  ZeebeClientBuilder zeebeClientBuilder;
 
   public static final ZeebeClientBuilderImpl DEFAULT =
     (ZeebeClientBuilderImpl) new ZeebeClientBuilderImpl().withProperties(new Properties());
@@ -29,6 +36,6 @@ public class ZeebeClientSpringConfiguration {
 
   @Bean
   public ZeebeClientObjectFactory zeebeClientObjectFactory() {
-    return () -> (ZeebeClientImpl) DEFAULT.build();
+    return () -> (ZeebeClientImpl) zeebeClientBuilder.build();
   }
 }
