ac420a8a4d794be2620077bc05628adf8b321124
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-02-03 15:01:51 +0100

    Improved test case

--- test/src/test/java/io/camunda/zeebe/spring/client/config/JobHandlerTest.java
+++ test/src/test/java/io/camunda/zeebe/spring/client/config/JobHandlerTest.java
@@ -23,6 +23,7 @@ import java.util.Map;
 
 import static io.camunda.zeebe.process.test.assertions.BpmnAssert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = {JobHandlerTest.class})
 @ZeebeSpringAssertions
@@ -35,11 +36,11 @@ public class JobHandlerTest {
   private InMemoryEngine engine;
 
   private static boolean calledTest1 = false;
+  private static boolean calledTest2 = false;
 
   @ZeebeWorker(name="test1", type = "test1", autoComplete = true)
   public void handleTest1(JobClient client, ActivatedJob job) {
     calledTest1 = true;
-    System.out.println("############################### Job handled");
   }
 
   @Test
@@ -50,20 +51,13 @@ public class JobHandlerTest {
       .endEvent()
       .done();
 
-    System.out.println("############################### Deploy instance...");
     client.newDeployCommand().addProcessModel(bpmnModel, "test1.bpmn").send().join();
 
-    final Map<String, Object> variables = Collections.singletonMap("magicNumber", "42"); // 42 instead of "42" fails?
-
-    // when
-    System.out.println("############################### Start instance...");
+    final Map<String, Object> variables = Collections.singletonMap("magicNumber", "42"); // Todo: 42 instead of "42" fails?
     ProcessInstanceEvent processInstance = startProcessInstance(client, "test1", variables);
 
-    // then
     assertThat(processInstance).isStarted();
-    System.out.println("############################### start waiting...");
     waitForCompletion(processInstance);
-    System.out.println("############################### ... finished waiting ");
     assertTrue(calledTest1);
   }
 
@@ -71,6 +65,7 @@ public class JobHandlerTest {
   public void handleTest2(JobClient client, ActivatedJob job) {
     // Complete it here to trigger a not found in the auto complete, which will be ignored
     client.newCompleteCommand(job.getKey()).send().join();
+    calledTest2 = true;
   }
 
   @Test
@@ -78,16 +73,10 @@ public class JobHandlerTest {
     BpmnModelInstance bpmnModel = Bpmn.createExecutableProcess("test2").startEvent().serviceTask().zeebeJobType("test2").endEvent().done();
     client.newDeployCommand().addProcessModel(bpmnModel, "test2.bpmn").send().join();
     ProcessInstanceEvent processInstance = startProcessInstance(client, "test2");
-
-    // then
     assertThat(processInstance).isStarted();
-    System.out.println("############################### start waiting...");
     waitForCompletion(processInstance);
-    System.out.println("############################### ... finished waiting ");
-    assertTrue(calledTest1);
+    assertTrue(calledTest2);
   }
-
-
 
   private ProcessInstanceEvent startProcessInstance(ZeebeClient client, String bpmnProcessId) {
     return startProcessInstance(client, bpmnProcessId, new HashMap<>());
@@ -100,10 +89,8 @@ public class JobHandlerTest {
   // TODO find a better solution for this
   public void waitForCompletion(ProcessInstanceEvent processInstance) {
     Awaitility.await().atMost(Duration.ofMillis(2000)).untilAsserted(() -> {
-      Thread.sleep(1000L);
       RecordStreamSourceStore.init(engine.getRecordStream());
       assertThat(processInstance).isCompleted();
-      Thread.sleep(500L);
     });
   }
 
