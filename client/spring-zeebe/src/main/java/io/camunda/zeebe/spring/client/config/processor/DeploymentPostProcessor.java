aca226e8c46c9316633683b7febabcfb6640ef18
lzgabel <lz19960321lz@gmail.com>
2022-04-13 21:36:39 +0800

    feat: use deploy resource rpc in deploy command

--- client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/config/processor/DeploymentPostProcessor.java
+++ client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/config/processor/DeploymentPostProcessor.java
@@ -2,25 +2,30 @@ package io.camunda.zeebe.spring.client.config.processor;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.command.DeployProcessCommandStep1;
+import io.camunda.zeebe.client.api.command.DeployResourceCommandStep1;
+import io.camunda.zeebe.client.api.response.Decision;
+import io.camunda.zeebe.client.api.response.DecisionRequirements;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
+import io.camunda.zeebe.client.api.response.Process;
 import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
 import io.camunda.zeebe.spring.client.bean.ClassInfo;
 import io.camunda.zeebe.spring.client.bean.value.ZeebeDeploymentValue;
 import io.camunda.zeebe.spring.client.bean.value.factory.ReadZeebeDeploymentValue;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.invoke.MethodHandles;
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DeploymentPostProcessor extends BeanInfoPostProcessor {
 
@@ -48,15 +53,15 @@ public class DeploymentPostProcessor extends BeanInfoPostProcessor {
 
     return client -> {
 
-      DeployProcessCommandStep1 deployProcessCommand = client
-        .newDeployCommand();
+      DeployResourceCommandStep1 deployResourceCommand = client
+        .newDeployResourceCommand();
 
       DeploymentEvent deploymentResult = value.getResources()
         .stream()
         .flatMap(resource -> Stream.of(getResources(resource)))
         .map(resource -> {
           try (InputStream inputStream = resource.getInputStream()) {
-            return deployProcessCommand.addResourceStream(inputStream, resource.getFilename());
+            return deployResourceCommand.addResourceStream(inputStream, resource.getFilename());
           } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
           }
@@ -69,10 +74,14 @@ public class DeploymentPostProcessor extends BeanInfoPostProcessor {
 
       LOGGER.info(
         "Deployed: {}",
-        deploymentResult
-          .getProcesses()
-          .stream()
-          .map(wf -> String.format("<%s:%d>", wf.getBpmnProcessId(), wf.getVersion()))
+        Stream.concat(deploymentResult
+            .getDecisionRequirements()
+            .stream()
+            .map(wf -> String.format("<%s:%d>", wf.getDmnDecisionRequirementsId(), wf.getVersion())),
+          deploymentResult
+            .getProcesses()
+            .stream()
+            .map(wf -> String.format("<%s:%d>", wf.getBpmnProcessId(), wf.getVersion())))
           .collect(Collectors.joining(",")));
     };
   }
