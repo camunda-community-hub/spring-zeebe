89b47bc5de60b79a087099e8e657dbd025970134
Igor Petrov <108870003+igpetrov@users.noreply.github.com>
2022-11-22 12:52:07 +0200

    fix: fixed comparator (#267)

--- client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/annotation/processor/OutboundConnectorAnnotationProcessor.java
+++ client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/annotation/processor/OutboundConnectorAnnotationProcessor.java
@@ -111,7 +111,7 @@ public class OutboundConnectorAnnotationProcessor extends AbstractZeebeAnnotatio
 
     @Override
     public int compare(OutboundConnectorConfiguration o1, OutboundConnectorConfiguration o2) {
-      if (o1 == o1) return 0;
+      if (o1 == o2) return 0;
       if (o1 == null) return -1;
 
       return o1.getType().compareTo(o2.getType());
