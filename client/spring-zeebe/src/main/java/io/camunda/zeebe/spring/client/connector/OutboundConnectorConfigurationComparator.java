71de26b6efe029c8a47b0a18373466307ca5ba4a
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-11-22 11:52:35 +0100

    fix comparator

--- client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/connector/OutboundConnectorConfigurationComparator.java
+++ client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/connector/OutboundConnectorConfigurationComparator.java
@@ -8,7 +8,7 @@ public class OutboundConnectorConfigurationComparator implements Comparator<Outb
 
   @Override
   public int compare(OutboundConnectorConfiguration o1, OutboundConnectorConfiguration o2) {
-    if (o1 == o1) return 0;
+    if (o1 == o2) return 0;
     if (o1 == null) return -1;
 
     return o1.getType().compareTo(o2.getType());
