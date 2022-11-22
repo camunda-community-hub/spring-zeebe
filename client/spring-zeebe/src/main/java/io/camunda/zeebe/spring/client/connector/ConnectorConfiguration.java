516cc075f27a1b3e3ccee2109d2a4a279538aa01
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-11-22 15:04:27 +0100

    Removed bean factory

--- client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/connector/ConnectorConfiguration.java
+++ client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/connector/ConnectorConfiguration.java
@@ -1,11 +1,9 @@
 package io.camunda.zeebe.spring.client.connector;
 
 import io.camunda.connector.api.secret.SecretProvider;
-import io.camunda.connector.impl.config.ConnectorBeanFactory;
 import io.camunda.connector.impl.config.ConnectorConfigurationUtil;
 import io.camunda.connector.impl.config.ConnectorPropertyResolver;
 import io.camunda.zeebe.spring.client.jobhandling.JobWorkerManager;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -27,13 +25,6 @@ public class ConnectorConfiguration {
     // Use static configuration mechanism, as connectors are independant of Spring
     ConnectorConfigurationUtil.setCustomPropertyResolver(springPropertyResolver);
     return springPropertyResolver;
-  }
-
-  @Bean
-  public ConnectorBeanFactory connectorBeanFactory(ApplicationContext context) {
-    SpringConnectorBeanFactory springConnectorBeanFactory = new SpringConnectorBeanFactory(context);
-    ConnectorConfigurationUtil.setCustomBeanFactory(springConnectorBeanFactory);
-    return springConnectorBeanFactory;
   }
 
 }
