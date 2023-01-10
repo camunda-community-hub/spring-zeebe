d0d5967a6e6fea5b006572a0d320b0b280b2f48b
Mark Farkas <119574841+markfarkas-camunda@users.noreply.github.com>
2023-01-10 11:12:53 +0100

    feat(metrics): collect execution time metrics (#304)
    * feat(metrics): collect execution time metrics
    
    * fix(metrics): execute the method in the default implementation of the MetricsRecorder

--- client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/metrics/SimpleMetricsRecorder.java
+++ client/spring-zeebe/src/main/java/io/camunda/zeebe/spring/client/metrics/SimpleMetricsRecorder.java
@@ -11,6 +11,8 @@ public class SimpleMetricsRecorder implements MetricsRecorder {
 
   public HashMap<String, AtomicLong> counters = new HashMap<>();
 
+  public HashMap<String, Long> timers = new HashMap<>();
+
   @Override
   public void increase(String metricName, String action, String type) {
     String key = key(metricName, action, type);
@@ -19,6 +21,13 @@ public class SimpleMetricsRecorder implements MetricsRecorder {
     } else {
       counters.get(key).incrementAndGet();
     }
+  }
+
+  @Override
+  public void executeWithTimer(String metricName, Runnable methodToExecute) {
+    long startTime = System.currentTimeMillis();
+    methodToExecute.run();
+    timers.put(metricName, System.currentTimeMillis() - startTime);
   }
 
   private String key(String metricName, String action, String type) {
@@ -31,5 +40,12 @@ public class SimpleMetricsRecorder implements MetricsRecorder {
       return 0;
     }
     return counters.get(key(metricName, action, type)).get();
+  }
+
+  public long getTimer(String metricName) {
+    if (!timers.containsKey(metricName)) {
+      return 0;
+    }
+    return counters.get(metricName).get();
   }
 }
