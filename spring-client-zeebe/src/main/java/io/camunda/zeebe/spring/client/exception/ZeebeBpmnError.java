e9c32770124d891b45f6077849e64f7b9f77ced3
pcln-jaynil-patel <jaynil.patel@priceline.com>
2023-08-23 13:16:23 -0400

    Add variables in ZeebeBpmnError

--- spring-client-zeebe/src/main/java/io/camunda/zeebe/spring/client/exception/ZeebeBpmnError.java
+++ spring-client-zeebe/src/main/java/io/camunda/zeebe/spring/client/exception/ZeebeBpmnError.java
@@ -1,5 +1,7 @@
 package io.camunda.zeebe.spring.client.exception;
 
+import java.util.Map;
+
 /**
  * Indicates an error in sense of BPMN occured, that should be handled by the BPMN process,
  * see https://docs.camunda.io/docs/reference/bpmn-processes/error-events/error-events/
@@ -8,11 +10,19 @@ public class ZeebeBpmnError extends RuntimeException {
 
   private String errorCode;
   private String errorMessage;
+  private Map<String, Object> variables;
 
   public ZeebeBpmnError(String errorCode, String errorMessage) {
     super("[" + errorCode + "] " + errorMessage);
     this.errorCode = errorCode;
     this.errorMessage = errorMessage;
+  }
+
+  public ZeebeBpmnError(String errorCode, String errorMessage, Map<String, Object> variables) {
+    super("[" + errorCode + "] " + errorMessage);
+    this.errorCode = errorCode;
+    this.errorMessage = errorMessage;
+    this.variables = variables;
   }
 
   public String getErrorMessage() {
@@ -21,5 +31,9 @@ public class ZeebeBpmnError extends RuntimeException {
 
   public String getErrorCode() {
     return errorCode;
+  }
+
+  public Map<String, Object> getVariables() {
+    return variables;
   }
 }
