4036ec7c35e84168e582d91e2ff44edec6d50240
1nb0und <133918079+1nb0und@users.noreply.github.com>
2023-11-20 22:46:16 -0500

    feat: using singleton for http client for auth

--- spring-client-common/src/main/java/io/camunda/commons/auth/SaaSAuthentication.java
+++ spring-client-common/src/main/java/io/camunda/commons/auth/SaaSAuthentication.java
@@ -6,7 +6,6 @@ import io.camunda.commons.json.SdkObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.slf4j.Logger;
@@ -57,7 +56,7 @@ public class SaaSAuthentication extends JwtAuthentication {
       TokenRequest tokenRequest = new TokenRequest(getAudience(product), credential.clientId, credential.clientSecret);
 
       httpPost.setEntity(new StringEntity(jsonMapper.toJson(tokenRequest)));
-      CloseableHttpClient client = HttpClients.createDefault();
+      CloseableHttpClient client = HttpClient.getInstance();
       CloseableHttpResponse response = client.execute(httpPost);
       TokenResponse tokenResponse = jsonMapper.fromJson(EntityUtils.toString(response.getEntity()), TokenResponse.class);
       tokens.put(product, tokenResponse.getAccessToken());
--- spring-client-common/src/main/java/io/camunda/commons/auth/SelfManagedAuthentication.java
+++ spring-client-common/src/main/java/io/camunda/commons/auth/SelfManagedAuthentication.java
@@ -5,7 +5,6 @@ import io.camunda.commons.json.SdkObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.slf4j.Logger;
@@ -87,7 +86,7 @@ public class SelfManagedAuthentication extends JwtAuthentication {
         .collect(Collectors.joining("&"));
 
       httpPost.setEntity(new StringEntity(form));
-      CloseableHttpClient client = HttpClients.createDefault();
+      CloseableHttpClient client = HttpClient.getInstance();
       CloseableHttpResponse response = client.execute(httpPost);
       TokenResponse tokenResponse =  jsonMapper.fromJson(EntityUtils.toString(response.getEntity()), TokenResponse.class);
       // TODO: verify JWT has the desired permission vs what the user requested
