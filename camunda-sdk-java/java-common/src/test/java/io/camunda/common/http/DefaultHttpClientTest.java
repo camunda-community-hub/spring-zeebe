e75051f8083d3844ba25a595d2201e77cc798466
1nb0und <133918079+1nb0und@users.noreply.github.com>
2023-11-26 17:51:53 -0800

    test: add more unit tests

--- camunda-sdk-java/java-common/src/test/java/io/camunda/common/http/DefaultHttpClientTest.java
+++ camunda-sdk-java/java-common/src/test/java/io/camunda/common/http/DefaultHttpClientTest.java
@@ -5,6 +5,7 @@ import io.camunda.common.auth.Authentication;
 import io.camunda.common.auth.Product;
 import io.camunda.common.json.JsonMapper;
 import io.camunda.common.json.SdkObjectMapper;
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -40,14 +41,14 @@ public class DefaultHttpClientTest {
     Map<Product, Map<Class<?>, String>> productMap = new HashMap<>();
     DefaultHttpClient defaultHttpClient = new DefaultHttpClient(authentication, chClient, jsonMapper, productMap);
     CloseableHttpResponse response = mock(CloseableHttpResponse.class, RETURNS_DEEP_STUBS);
-    MyTestClass expectedOutput = new MyTestClass();
+    MyResponseClass expectedOutput = new MyResponseClass();
     expectedOutput.setName("test-name");
 
     // when
     when(chClient.execute(any(HttpGet.class))).thenReturn(response);
     when(authentication.getTokenHeader(any())).thenReturn(new AbstractMap.SimpleEntry<>("key", "value"));
     when(response.getEntity().getContent()).thenReturn(new ByteArrayInputStream("{\"name\" : \"test-name\"}".getBytes()));
-    MyTestClass parsedResponse = defaultHttpClient.get(MyTestClass.class, "123");
+    MyResponseClass parsedResponse = defaultHttpClient.get(MyResponseClass.class, "123");
 
     // then
     assertTrue(new ReflectionEquals(expectedOutput).matches(parsedResponse));
@@ -59,26 +60,49 @@ public class DefaultHttpClientTest {
     Map<Product, Map<Class<?>, String>> productMap = new HashMap<>();
     DefaultHttpClient defaultHttpClient = new DefaultHttpClient(authentication, chClient, jsonMapper, productMap);
     CloseableHttpResponse response = mock(CloseableHttpResponse.class, RETURNS_DEEP_STUBS);
-    MyTestClass insideClass = new MyTestClass();
+    MyResponseClass insideClass = new MyResponseClass();
     insideClass.setName("test-name");
-    List<MyTestClass> expectedOutput = new ArrayList<>();
+    List<MyResponseClass> expectedOutput = new ArrayList<>();
     expectedOutput.add(insideClass);
 
     // when
     when(chClient.execute(any(HttpPost.class))).thenReturn(response);
     when(authentication.getTokenHeader(any())).thenReturn(new AbstractMap.SimpleEntry<>("key", "value"));
     when(response.getEntity().getContent()).thenReturn(new ByteArrayInputStream("[{\"name\" : \"test-name\"}]".getBytes()));
-    List parsedResponse = defaultHttpClient.post(List.class, MyTestClass.class, TypeToken.of(MyTestClass.class), "dummy");
+    List parsedResponse = defaultHttpClient.post(List.class, MyResponseClass.class, TypeToken.of(MyResponseClass.class), "dummy");
 
     // then
     assertEquals(expectedOutput.size(), parsedResponse.size());
     assertTrue(new ReflectionEquals(expectedOutput.get(0)).matches(parsedResponse.get(0)));
   }
 
-  public static class MyTestClass {
+  @Test
+  public void shouldReturnDeleteType() throws IOException {
+    // given
+    Map<Product, Map<Class<?>, String>> productMap = new HashMap<>();
+    DefaultHttpClient defaultHttpClient = new DefaultHttpClient(authentication, chClient, jsonMapper, productMap);
+    CloseableHttpResponse response = mock(CloseableHttpResponse.class, RETURNS_DEEP_STUBS);
+    MyResponseClass expectedOutput = new MyResponseClass();
+    expectedOutput.setName("test-name");
+
+    // when
+    when(chClient.execute(any(HttpDelete.class))).thenReturn(response);
+    when(authentication.getTokenHeader(any())).thenReturn(new AbstractMap.SimpleEntry<>("key", "value"));
+    when(response.getEntity().getContent()).thenReturn(new ByteArrayInputStream("{\"name\" : \"test-name\"}".getBytes()));
+    MyResponseClass parsedResponse = defaultHttpClient.delete(MySelectorClass.class, MyResponseClass.class, 123L);
+
+    // then
+    assertTrue(new ReflectionEquals(expectedOutput).matches(parsedResponse));
+  }
+
+  // TODO: add more tests for retrieving the right url path based on input
+
+  public static class MyResponseClass {
     private String name;
     public void setName(String name) {
       this.name = name;
     }
   }
+
+  public static class MySelectorClass { }
 }
