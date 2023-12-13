7b656af00adb80c55006e9cc545623f918fa4297
1nb0und <133918079+1nb0und@users.noreply.github.com>
2023-12-13 10:26:17 -0500

    feat: auto recognize operate props

--- spring-boot-starter-camunda/src/main/java/io/camunda/zeebe/spring/client/properties/OperateClientConfigurationProperties.java
+++ spring-boot-starter-camunda/src/main/java/io/camunda/zeebe/spring/client/properties/OperateClientConfigurationProperties.java
@@ -1,6 +1,7 @@
 package io.camunda.zeebe.spring.client.properties;
 
 import io.camunda.zeebe.spring.client.properties.common.Client;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,31 +14,36 @@ public class OperateClientConfigurationProperties extends Client {
 
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+  // Normal Zeebe Engine Properties
+  @Value("${zeebe.client.cloud.cluster-id:#{null}}")
+  private String clusterId;
 
-    // Normal Zeebe Engine Properties
-    @Value("${zeebe.client.cloud.cluster-id:#{null}}")
-    private String clusterId;
+  @Value("${zeebe.client.cloud.region:bru-2}")
+  private String region;
 
-    @Value("${zeebe.client.cloud.region:bru-2}")
-    private String region;
+  // TODO: This currently assumes PROD in Cloud - do we want to support DEV and INT?
+  // and make it configurable? At the moment the workaround is to set the operateUrl yourself
+  public static String operateCloudBaseUrl = "operate.camunda.io";
 
-    @Value("${camunda.operate.client.url:#{null}}")
-    private String operateUrl;
-
-    // TODO: This currently assumes PROD in Cloud - do we want to support DEV and INT?
-    // and make it configurable? At the moment the workaround is to set the operateUrl yourself
-    public static String operateCloudBaseUrl = "operate.camunda.io";
-
-    public String getOperateUrl() {
-      if (operateUrl != null) {
-        LOG.debug("Connecting to Camunda Operate on URL: " + operateUrl);
-        return operateUrl;
-      } else if (clusterId != null) {
-        String url = "https://" + region + "." + operateCloudBaseUrl + "/" + clusterId + "/";
-        LOG.debug("Connecting to Camunda Operate SaaS via URL: " + url);
-        return url;
-      }
-      throw new IllegalArgumentException(
-        "In order to connect to Camunda Operate you need to specify either a SaaS clusterId or an Operate URL.");
+  public String getOperateUrl() {
+    if (getUrl() != null) {
+      LOG.debug("Connecting to Camunda Operate on URL: " + getUrl());
+      return getUrl();
+    } else if (clusterId != null) {
+      String url = "https://" + region + "." + operateCloudBaseUrl + "/" + clusterId + "/";
+      LOG.debug("Connecting to Camunda Operate SaaS via URL: " + url);
+      return url;
     }
+    throw new IllegalArgumentException(
+      "In order to connect to Camunda Operate you need to specify either a SaaS clusterId or an Operate URL.");
+  }
+
+  @PostConstruct
+  private void applyFinalValues() {
+      if (this.getClientId() != null && this.getClientSecret() != null) {
+        this.setEnabled(true);
+      } else if (this.getUsername() != null && this.getPassword() != null) {
+        this.setEnabled(true);
+      }
+  }
 }
