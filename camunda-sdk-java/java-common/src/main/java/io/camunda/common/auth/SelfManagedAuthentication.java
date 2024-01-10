38e137dd6699f51d09710c5d259f691768ddf942
1nb0und <133918079+1nb0und@users.noreply.github.com>
2024-01-10 11:47:26 -0500

    feat: remove cache http client
    (cherry picked from commit d6028be532a8c2cbb4e83da4a7bbd4d303472f0e)

--- camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/SelfManagedAuthentication.java
+++ camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/SelfManagedAuthentication.java
@@ -6,6 +6,7 @@ import io.camunda.common.json.SdkObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
@@ -81,7 +82,7 @@ public class SelfManagedAuthentication extends JwtAuthentication {
   }
 
   private String retrieveToken(Product product, JwtCredential jwtCredential) {
-    try(CloseableHttpClient client = HttpClient.getInstance()) {
+    try(CloseableHttpClient client = HttpClients.createDefault()) {
       HttpPost request = buildRequest(jwtCredential);
       TokenResponse tokenResponse =
           client.execute(
