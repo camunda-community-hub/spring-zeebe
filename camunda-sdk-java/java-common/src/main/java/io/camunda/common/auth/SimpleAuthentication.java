133c4a8ad962c61cacaf9b61d58470fd64744017
sbuettner <simon.buettner@camunda.com>
2024-01-10 11:20:26 +0100

    fix(http): Close the http client resources in the SDK authentication

--- camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/SimpleAuthentication.java
+++ camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/SimpleAuthentication.java
@@ -48,22 +48,20 @@ public class SimpleAuthentication implements Authentication {
   }
 
   private String retrieveToken(Product product, SimpleCredential simpleCredential) {
-    try {
-      HttpPost httpPost = new HttpPost(authUrl);
-      List<NameValuePair> params = new ArrayList<>();
-      params.add(new BasicNameValuePair("username", simpleCredential.getUser()));
-      params.add(new BasicNameValuePair("password", simpleCredential.getPassword()));
-      httpPost.setEntity(new UrlEncodedFormEntity(params));
-
-      CloseableHttpClient client = HttpClient.getInstance();
-      CloseableHttpResponse response = client.execute(httpPost);
-      Header[] cookieHeaders = response.getHeaders("Set-Cookie");
-      String cookie = null;
-      for (Header cookieHeader : cookieHeaders) {
-        if (cookieHeader.getValue().startsWith("OPERATE-SESSION")) {
-          cookie = response.getHeader("Set-Cookie").getValue();
+    try(CloseableHttpClient client = HttpClient.getInstance()) {
+      HttpPost request = buildRequest(simpleCredential);
+      String cookie = client.execute(request, response -> {
+        Header[] cookieHeaders = response.getHeaders("Set-Cookie");
+        String cookieCandidate = null;
+        String cookiePrefix = product.toString().toUpperCase() + "-SESSION";
+        for (Header cookieHeader : cookieHeaders) {
+          if (cookieHeader.getValue().startsWith(cookiePrefix)) {
+            cookieCandidate = response.getHeader("Set-Cookie").getValue();
+            break;
+          }
         }
-      }
+        return cookieCandidate;
+      });
       if (cookie == null) {
         throw new RuntimeException("Unable to authenticate due to missing Set-Cookie");
       }
@@ -75,6 +73,14 @@ public class SimpleAuthentication implements Authentication {
     return tokens.get(product);
   }
 
+  private HttpPost buildRequest(SimpleCredential simpleCredential) {
+    HttpPost httpPost = new HttpPost(authUrl);
+    List<NameValuePair> params = new ArrayList<>();
+    params.add(new BasicNameValuePair("username", simpleCredential.getUser()));
+    params.add(new BasicNameValuePair("password", simpleCredential.getPassword()));
+    httpPost.setEntity(new UrlEncodedFormEntity(params));
+    return httpPost;
+  }
 
     @Override
   public Map.Entry<String, String> getTokenHeader(Product product) {
