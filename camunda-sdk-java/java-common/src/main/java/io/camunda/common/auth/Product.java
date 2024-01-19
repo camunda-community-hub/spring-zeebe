fffbaa23ed6c72636feae747542d10986959c36e
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-01-19 14:38:02 +0100

    Update Product.java (#603)
    (cherry picked from commit e8fffa44d9cf287914c224a2e57a22e49ffa570c)

--- camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/Product.java
+++ camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/Product.java
@@ -4,5 +4,5 @@ package io.camunda.common.auth;
  * Enum for different C8 Products
  */
 public enum Product {
-  ZEEBE, OPERATE, TASKLIST, CONSOLE, OPTIMIZE
+  ZEEBE, OPERATE, TASKLIST, CONSOLE, OPTIMIZE, WEB_MODELER
 }
