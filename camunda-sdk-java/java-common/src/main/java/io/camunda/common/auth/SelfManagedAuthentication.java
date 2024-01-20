7353c90a0f0663e6304f62eeb5d4394690c24c65
1nb0und <133918079+1nb0und@users.noreply.github.com>
2024-01-19 19:48:47 -0500

    feat: remove dead code

--- camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/SelfManagedAuthentication.java
+++ camunda-sdk-java/java-common/src/main/java/io/camunda/common/auth/SelfManagedAuthentication.java
@@ -28,20 +28,9 @@ import java.util.stream.Collectors;
 public class SelfManagedAuthentication extends JwtAuthentication {
 
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-  private String authUrl;
-
-  // TODO: Check with Identity about upcoming IDPs to abstract this
-//  private String keycloakRealm = "camunda-platform";
-//  private String keycloakUrl;
-//  private String keycloakTokenUrl;
   private JwtConfig jwtConfig;
-
   private IdentityConfig identityConfig;
   private Map<Product, String> tokens;
-
-  // TODO: have a single object mapper to be used all throughout the SDK, i.e.bean injection
-  private JsonMapper jsonMapper = new SdkObjectMapper();
 
   public SelfManagedAuthentication() {
     tokens = new HashMap<>();
@@ -50,18 +39,6 @@ public class SelfManagedAuthentication extends JwtAuthentication {
   public static SelfManagedAuthenticationBuilder builder() {
     return new SelfManagedAuthenticationBuilder();
   }
-
-//  public void setKeycloakRealm(String keycloakRealm) {
-//    this.keycloakRealm = keycloakRealm;
-//  }
-
-//  public void setKeycloakUrl(String keycloakUrl) {
-//    this.keycloakUrl = keycloakUrl;
-//  }
-//
-//  public void setKeycloakTokenUrl(String keycloakTokenUrl) {
-//    this.keycloakTokenUrl = keycloakTokenUrl;
-//  }
 
   public JwtConfig getJwtConfig() {
     return jwtConfig;
@@ -77,56 +54,8 @@ public class SelfManagedAuthentication extends JwtAuthentication {
 
   @Override
   public Authentication build() {
-//    if (keycloakTokenUrl != null) {
-//      authUrl = keycloakTokenUrl;
-//    } else {
-//      authUrl = keycloakUrl+"/auth/realms/"+keycloakRealm+"/protocol/openid-connect/token";
-//    }
     return this;
   }
-
-//  @Override
-//  public void resetToken(Product product) {
-//    tokens.remove(product);
-//  }
-
-//  private String retrieveToken(Product product, JwtCredential jwtCredential) {
-//    try {
-//      HttpPost httpPost = new HttpPost(authUrl);
-//      httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
-//
-//      Map<String, String> parameters = new HashMap<>();
-//      parameters.put("grant_type", "client_credentials");
-//      parameters.put("client_id", jwtCredential.getClientId());
-//      parameters.put("client_secret", jwtCredential.getClientSecret());
-//
-//      String form = parameters.entrySet()
-//        .stream()
-//        .map(e -> {
-//          try {
-//            return e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8.toString());
-//          } catch (UnsupportedEncodingException ex) {
-//            throw new RuntimeException(ex);
-//          }
-//        })
-//        .collect(Collectors.joining("&"));
-//
-//      httpPost.setEntity(new StringEntity(form));
-//      CloseableHttpClient client = HttpClient.getInstance();
-//      CloseableHttpResponse response = client.execute(httpPost);
-//      TokenResponse tokenResponse;
-//      if (response.getCode() == HttpStatus.SC_OK) {
-//        tokenResponse =  jsonMapper.fromJson(EntityUtils.toString(response.getEntity()), TokenResponse.class);
-//      } else {
-//        throw new SdkException("Error "+response.getCode()+" obtaining access token: "+EntityUtils.toString(response.getEntity()));
-//      }
-//      tokens.put(product, tokenResponse.getAccessToken());
-//    } catch (Exception e) {
-//      LOG.error("Authenticating for " + product + " failed due to " + e);
-//      throw new SdkException("Unable to authenticate", e);
-//    }
-//    return tokens.get(product);
-//  }
 
   @Override
   public void resetToken(Product product) {
@@ -143,8 +72,6 @@ public class SelfManagedAuthentication extends JwtAuthentication {
       String audience = jwtConfig.getProduct(product).getAudience();
       Tokens identityTokens = identity.authentication().requestToken(audience);
       token = identityTokens.getAccessToken();
-      // TODO: remove this
-      System.out.println("Token: " + token);
     }
     return new AbstractMap.SimpleEntry<>("Authorization", "Bearer " + token);
   }
