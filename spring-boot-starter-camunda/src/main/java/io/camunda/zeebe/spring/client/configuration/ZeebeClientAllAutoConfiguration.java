f14ac8f7cbd13e8c555cbc5c1a7893a52666d105
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-02-14 12:19:19 +0100

    made exector service configurable again

--- spring-boot-starter-camunda/src/main/java/io/camunda/zeebe/spring/client/configuration/ZeebeClientAllAutoConfiguration.java
+++ spring-boot-starter-camunda/src/main/java/io/camunda/zeebe/spring/client/configuration/ZeebeClientAllAutoConfiguration.java
@@ -39,7 +39,8 @@ public class ZeebeClientAllAutoConfiguration {
   @Bean
   @ConditionalOnMissingBean
   public ZeebeClientExecutorService zeebeClientExecutorService() {
-    return ZeebeClientExecutorService.createDefault();
+    return ZeebeClientExecutorService.createDefault(
+        configurationProperties.getNumJobWorkerExecutionThreads());
   }
 
   @Bean
