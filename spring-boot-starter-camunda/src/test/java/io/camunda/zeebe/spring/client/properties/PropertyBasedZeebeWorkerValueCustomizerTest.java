6f5f306e3e1425f8f1cec0ac7d354a25b8729ad3
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-02-16 13:15:54 +0100

    formatter

--- spring-boot-starter-camunda/src/test/java/io/camunda/zeebe/spring/client/properties/PropertyBasedZeebeWorkerValueCustomizerTest.java
+++ spring-boot-starter-camunda/src/test/java/io/camunda/zeebe/spring/client/properties/PropertyBasedZeebeWorkerValueCustomizerTest.java
@@ -1,17 +1,18 @@
 package io.camunda.zeebe.spring.client.properties;
 
+import static org.assertj.core.api.Assertions.*;
+
 import io.camunda.zeebe.spring.client.annotation.value.ZeebeWorkerValue;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-
 public class PropertyBasedZeebeWorkerValueCustomizerTest {
   @Test
-  void shouldApplyOverrides(){
+  void shouldApplyOverrides() {
     ZeebeClientConfigurationProperties properties = new ZeebeClientConfigurationProperties(null);
     properties.applyOverrides();
-    properties.getWorker().getOverride().put("someWorker",new ZeebeWorkerValue().enabled(false));
-    PropertyBasedZeebeWorkerValueCustomizer customizer = new PropertyBasedZeebeWorkerValueCustomizer(properties);
+    properties.getWorker().getOverride().put("someWorker", new ZeebeWorkerValue().enabled(false));
+    PropertyBasedZeebeWorkerValueCustomizer customizer =
+        new PropertyBasedZeebeWorkerValueCustomizer(properties);
     ZeebeWorkerValue original = new ZeebeWorkerValue().type("someWorker").enabled(true);
     customizer.customize(original);
     assertThat(original.getEnabled()).isEqualTo(false);
