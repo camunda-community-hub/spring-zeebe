f5fb1a4c00015b8cef0a61e3c388027cfa568453
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-07-03 11:31:07 +0200

    feat(simple-authentication): add the management of CSRF token for the simple authentication (#860) (#861)

--- spring-boot-starter-camunda/src/test/java/io/camunda/zeebe/spring/client/config/AuthenticationConfigurationSimpleTest.java
+++ spring-boot-starter-camunda/src/test/java/io/camunda/zeebe/spring/client/config/AuthenticationConfigurationSimpleTest.java
@@ -8,6 +8,8 @@ import io.camunda.common.auth.Authentication;
 import io.camunda.common.auth.Product;
 import io.camunda.common.auth.SimpleAuthentication;
 import io.camunda.zeebe.spring.client.configuration.AuthenticationConfiguration;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +30,11 @@ public class AuthenticationConfigurationSimpleTest {
     assertThat(authentication).isExactlyInstanceOf(SimpleAuthentication.class);
   }
 
+  @BeforeEach
+  void setUp() {
+    authentication.resetToken(Product.OPERATE);
+  }
+
   @Test
   void shouldHaveOperateAuth() {
     stubFor(
@@ -39,7 +46,10 @@ public class AuthenticationConfigurationSimpleTest {
                         "OPERATE-X-CSRF-TOKEN=139196d4-7768-451c-aa66-078e1ed74785")));
     assertThat(authentication.getTokenHeader(Product.OPERATE))
         .isNotNull()
-        .isEqualTo(entry("Cookie", "OPERATE-SESSION=3205A03818447100591792E774DB8AF6"));
+        .isEqualTo(
+            Map.of(
+                "Cookie",
+                "OPERATE-SESSION=3205A03818447100591792E774DB8AF6; OPERATE-X-CSRF-TOKEN=139196d4-7768-451c-aa66-078e1ed74785"));
     verify(
         postRequestedFor(urlEqualTo("/api/login"))
             .withHeader(
@@ -57,7 +67,32 @@ public class AuthenticationConfigurationSimpleTest {
                         "OPERATE-X-CSRF-TOKEN=139196d4-7768-451c-aa66-078e1ed74785")));
     assertThat(authentication.getTokenHeader(Product.TASKLIST))
         .isNotNull()
-        .isEqualTo(entry("Cookie", "TASKLIST-SESSION=3205A03818447100591792E774DB8AF6"));
+        .isEqualTo(Map.of("Cookie", "TASKLIST-SESSION=3205A03818447100591792E774DB8AF6"));
+    verify(
+        postRequestedFor(urlEqualTo("/api/login"))
+            .withHeader(
+                "Content-Type", equalTo("application/x-www-form-urlencoded; charset=ISO-8859-1")));
+  }
+
+  @Test
+  void shouldHaveCSRFToken() {
+    stubFor(
+        post("/api/login")
+            .willReturn(
+                ok().withHeader(
+                        "Set-Cookie", "OPERATE-X-CSRF-TOKEN=139196d4-7768-451c-aa66-078e1ed74785")
+                    .withHeader(
+                        "OPERATE-X-CSRF-TOKEN",
+                        "WwbfQ33kHNEHu99ioC39yCMuVE2JjQnK_vYEpGPQGxBv1Nfn10pSDzapEpzfcJJvbiE2kG6jDMbFVVBSMy9K_K5dlV1")));
+
+    assertThat(authentication.getTokenHeader(Product.OPERATE))
+        .isNotNull()
+        .isEqualTo(
+            Map.of(
+                "Cookie",
+                "OPERATE-X-CSRF-TOKEN=139196d4-7768-451c-aa66-078e1ed74785",
+                "OPERATE-X-CSRF-TOKEN",
+                "WwbfQ33kHNEHu99ioC39yCMuVE2JjQnK_vYEpGPQGxBv1Nfn10pSDzapEpzfcJJvbiE2kG6jDMbFVVBSMy9K_K5dlV1"));
     verify(
         postRequestedFor(urlEqualTo("/api/login"))
             .withHeader(
